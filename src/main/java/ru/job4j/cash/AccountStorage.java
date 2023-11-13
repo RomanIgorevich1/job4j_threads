131107948ba706f1e2fe57fbf2b6c03c9bed8aef
votmo <votmoyapochta1@bk.ru>
2023-11-13 15:02:23 +0300

    Денежные переводы AccountStorage

--- src/main/java/ru/job4j/cash/AccountStorage.java
+++ src/main/java/ru/job4j/cash/AccountStorage.java
@@ -11,69 +11,37 @@ public class AccountStorage {
     @GuardedBy("accounts")
     private final HashMap<Integer, Account> accounts = new HashMap<>();
 
-    public boolean add(Account account) {
-        boolean check = false;
-        synchronized (accounts) {
-            if (!accounts.containsValue(account)) {
-                accounts.put(account.id(), account);
-                check = true;
-            }
-        }
-        return check;
+    public synchronized boolean add(Account account) {
+        accounts.putIfAbsent(account.id(), new Account(account.id(), account.amount()));
+        return true;
     }
 
-    public boolean update(Account account) {
-        boolean result2 = false;
-        synchronized (accounts) {
-            Optional<Account> result = getById(account.id());
-            if (result.isPresent()) {
-                accounts.put(account.id(), new Account(result.get().id(), account.amount()));
-                result2 = true;
-            }
-        }
-        return result2;
+    public synchronized boolean update(Account account) {
+        return accounts.replace(account.id(),
+                getById(account.id()).get(),
+                new Account(account.id(), account.amount()));
     }
 
-    public void delete(int id) {
-        Optional<Account> result = getById(id);
-        synchronized (accounts) {
-            result.ifPresent(account -> accounts.remove(account.id()));
-        }
+    public synchronized void delete(int id) {
+        getById(id).ifPresent(account -> accounts.remove(account.id()));
     }
 
-    public Optional<Account> getById(int id) {
-        Optional<Account> result = Optional.empty();
-        synchronized (accounts) {
-            if (accounts.containsKey(id)) {
-                result = accounts.values().stream().filter(num -> num.id() == id).findAny();
-            }
-        }
-        return result;
+    public synchronized Optional<Account> getById(int id) {
+        return Optional.ofNullable(accounts.get(id));
     }
 
     /**
      * Переводит сумму amount со счета fromId в счет toId
      * @param fromId с какого
-     * @param toId куда
+     * @param toId   куда
      * @param amount сумма
      * @return
      */
-    public  boolean transfer(int fromId, int toId, int amount) {
-        Optional<Account> firstAccount = getById(fromId);
-        Optional<Account> secondAccount = getById(toId);
-        boolean result = false;
-        synchronized (accounts) {
-            if (firstAccount.isPresent() && secondAccount.isPresent()) {
-                if (firstAccount.get().amount() == 0 || secondAccount.get().amount() == 0) {
-                    throw new IllegalArgumentException("No money to transfer");
-                }
-                accounts.put(firstAccount.get().id(),
-                        new Account(firstAccount.get().id(), firstAccount.get().amount() - amount));
-                accounts.put(secondAccount.get().id(),
-                        new Account(secondAccount.get().id(), secondAccount.get().amount() + amount));
-                result = true;
-            }
+    public synchronized boolean transfer(int fromId, int toId, int amount) {
+        if (getById(fromId).get().amount() == 0 || getById(toId).get().amount() == 0) {
+            throw new IllegalArgumentException("No money to transfer");
         }
-        return result;
+        update(new Account(getById(fromId).get().id(), getById(fromId).get().amount() - amount));
+        return update(new Account(getById(toId).get().id(), getById(toId).get().amount() + amount));
     }
 }
