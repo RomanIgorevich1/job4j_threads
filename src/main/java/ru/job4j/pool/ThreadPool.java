2daf20e93f32e2d4633e8ef99e00e3d2a7eb1dd9
votmo <votmoyapochta1@bk.ru>
2024-02-12 12:46:07 +0300

    Реализовать ThreadPool

---
+++ src/main/java/ru/job4j/pool/ThreadPool.java
@@ -0,0 +1,50 @@
+package ru.job4j.pool;
+
+import ru.job4j.blockingqueue.SimpleBlockingQueue;
+
+
+import java.util.LinkedList;
+import java.util.List;
+
+public class ThreadPool {
+    private final List<Thread> threads = new LinkedList<>();
+    private final int size = Runtime.getRuntime().availableProcessors();
+    private final SimpleBlockingQueue<Runnable> tasks =
+            new SimpleBlockingQueue<>(10);
+
+    public ThreadPool()  {
+        for (int i = 0; i <= size; i++) {
+            Thread thread = new Thread(() -> {
+                try {
+                    tasks.poll();
+                } catch (InterruptedException exception) {
+                    Thread.currentThread().interrupt();
+                }
+            });
+            thread.start();
+            threads.add(thread);
+        }
+    }
+
+    /**
+     * Метод добавляет задачи в блокирующую очередь
+     */
+    public void work(Runnable job) throws InterruptedException {
+        tasks.offer(job);
+    }
+
+    /**
+     * Метод завершает все запущенные задачи
+     */
+    public void shutdown() {
+        threads.forEach(Thread::interrupt);
+    }
+
+    public static void main(String[] args) throws InterruptedException {
+        ThreadPool pool = new ThreadPool();
+        for (int i = 0; i < 10; i++) {
+            pool.work(() -> System.out.println("The work  begins!!"));
+        }
+        pool.shutdown();
+    }
+}
