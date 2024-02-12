4b8ec200a0a72e42cc619826563f87eb812525c3
votmo <votmoyapochta1@bk.ru>
2024-02-12 17:04:47 +0300

    ExecutorService рассылка почты

--- src/main/java/ru/job4j/pool/EmailNotification.java
+++ src/main/java/ru/job4j/pool/EmailNotification.java
@@ -5,7 +5,7 @@ import java.util.concurrent.Executors;
 
 public class EmailNotification {
 
-    private ExecutorService pool = Executors.newCachedThreadPool();
+    private final ExecutorService pool = Executors.newCachedThreadPool();
 
     public void emailTo(User user) {
         pool.submit(() -> {
@@ -15,11 +15,17 @@ public class EmailNotification {
         });
         pool.submit(() -> send(String.format("Notification {%s} to email {%s}", user.username(), user.email()),
                 String.format("Add a new event to {%s}", user.username()), user.email()));
-        close();
     }
 
     public void close() {
         pool.shutdown();
+        while (!pool.isTerminated()) {
+            try {
+                Thread.sleep(100);
+            } catch (InterruptedException exception) {
+                Thread.currentThread().interrupt();
+            }
+        }
     }
 
     public void send(String subject, String body, String email) {
@@ -29,5 +35,6 @@ public class EmailNotification {
     public static void main(String[] args) {
         EmailNotification email = new EmailNotification();
         email.emailTo(new User("Roman", "Roman@yandex.ru"));
+        email.close();
     }
 }
